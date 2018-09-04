f64a3db3a5f4ebd9262859b1500228e73a89f319
Sergey Arkhipov <sergeyarkhipov@Serg.local>
2018-09-04 18:07:49 +0300

    header start

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,15 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = ({ siteTitle }) => (
- <div>
-   Header
+ <div className="Header"> 
+  <div className="HeaderGroup">
+  <link to="/" > <img src={require('../images/logo-designcode.svg')} width="30"/></link>
+  <link t="/courses">Courses</link>
+  <link t="/courses">Downloads</link>
+  <link t="/courses">Workshops</link>
+  <link t="/courses"><button>Buy</button></link>
+  </div>
+   
  </div>
 )
 
